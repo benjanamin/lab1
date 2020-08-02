cac7fc5951befe92f1742d1b1a19ef5c1bd45f92
Benjamin Castillo <benjamin.castillo.l>
2020-08-02 11:40:05 -0400

    .h agregado para matrix

---
+++ incl/matrix.h
@@ -0,0 +1,11 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "struct.h"
+
+MatrizGrafo* generarMatrizGrafo(int vertices);
+void imprimirMatrizAdyacencia(MatrizGrafo* matriz);
+MatrizGrafo* abrirArchivoMatriz(char *nombreArchivo);
+MatrizGrafo* eliminarCamino(int k, int l, MatrizGrafo* matrix);
+
+#endif
---
+++ incl/struct.h
@@ -0,0 +1,9 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct matrizGrafo {
+	int vertices;
+	int** adyacencias;
+}MatrizGrafo;
+
+#endif
